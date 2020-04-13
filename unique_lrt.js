681f34876e536d15ec725a74afbcf20bd64e5066
Krishna Kompalli <31897761+killerfizh@users.noreply.github.com>
2020-04-13 21:07:29 +0530

    Update unique_lrt.js

--- unique_lrt.js
+++ unique_lrt.js
@@ -5,16 +5,9 @@ ip.on('line', (ans) => {
     a = ans.split('');
 });
 function unique_letter(str){
-    var unq = [];
-    var unique;
- for(i of new Set(str)){
-    unq.push(i)
- }
-unique = unq.join('')
-var count = 0;
 var res = 0;
 for(i=0; i<str.length; i++){
-    count = 0;
+    var count = 0;
     for (j=0; j<str.length; j++){
     if (str[i]==str[j]){
         count++

1e554c9d1f45ab57bb0b6adca9b83b348ec41a72
Krishna Kompalli <31897761+killerfizh@users.noreply.github.com>
2020-04-13 17:20:20 +0530

    Create min_ele_in_lst.js

---
+++ min_ele_in_lst.js
@@ -0,0 +1,23 @@
+const rl = require('readline');
+const ip = rl.createInterface({input:process.stdin, output:process.stdout});
+var a,b;
+var count = 0;
+ip.on('line', (ans) => {
+    if (count === 0){
+        a = parseInt(ans);
+        count++;
+    }
+    else if (count ==1){
+        b = ans.split(' ').map(n => Number(n)).splice(0,a+1);
+        count++;
+    }
+    else{
+        return
+    }
+
+});
+function minele(lst){
+    return lst.sort(function(a,b){return a-b})[0]
+}
+
+ip.on('close', () => {console.log(minele(b))})
